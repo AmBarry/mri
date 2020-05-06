02d07dcf5bd2e63d0429672bb68fc094cf3d1baf
Cristian Groza <cgroza@bougainvillier.local>
2020-05-05 20:35:44 -0400

    Color UMAP by sex

--- plot_umap.R
+++ plot_umap.R
@@ -1,12 +1,26 @@
 library(tidyverse)
+library(viridis)
 
+umap <- read_csv("umap_embedding.csv", col_names = c("UMAP1", "UMAP2"))
 
-umap <- read_csv("umap_embeddings.csv", col_names = c("UMAP1", "UMAP2"))
+labels <- read_csv("umap_labels.txt", col_names = "surferID") %>%
+  mutate(SUB_ID = as.integer(str_extract(surferID, pattern = "\\d+")))
+umap <- umap %>% add_column(SUB_ID = labels$SUB_ID)
 
+metadata <- read_csv("ABIDE_Phenotype.csv", na = c("", "NA", -9999))
 
-labels <- read_csv("umap_labels.txt")
+d <- inner_join(metadata, umap, by = "SUB_ID")
+d$SEX <- as.factor(d$SEX)
 
 theme_set(theme_bw())
-ggplot(umap) + geom_point(aes(x=UMAP1, y=UMAP2)) + ggtitle("UMAP on 1052 brains") + theme(panel.grid = element_blank())
+ggplot(d) + geom_point(aes(x=UMAP1, y=UMAP2, color = SEX)) +
+  ggtitle("UMAP on 1052 brains") +
+  theme(panel.grid = element_blank())
 ggsave("brains_map.pdf")
 ggsave("brains_map.png")
+
+ggplot(d) +
+  geom_text(aes(x=UMAP1, y=UMAP2, label =  SUB_ID), size = 1, check_overlap = T) +
+  ggtitle("UMAP on 1052 brains") +
+  theme(panel.grid = element_blank())
+ggsave("brains_map_byid.pdf")
